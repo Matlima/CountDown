82aaab094212b14c68823472a9932a81f9c4d026
Matheus <limarios18@outlook.com>
2024-03-05 23:07:45 -0300

    Criando logica da contagem regressiva com hooks

--- src/hooks/useCountdown.jsx
+++ src/hooks/useCountdown.jsx
@@ -4,7 +4,7 @@ const useCountdown = (date) => {
     const [day, setDay] = useState();
     const [hour, setHour] = useState();
     const [minute, setMinute] = useState();
-    const [secund, setSecund] = useState();
+    const [second, setSecond] = useState();
 
     const countdown = () => {
         const countDate = new Date(date).getTime();
@@ -12,22 +12,25 @@ const useCountdown = (date) => {
 
         const interval = countDate - now;
 
-        const secund = 1000;
-        const minute = secund * 60;
+        const second = 1000;
+        const minute = second * 60;
         const hour = minute * 60;
         const day = hour * 24;
 
         const dayNumber = Math.floor(interval / day);
         const hourNumber = Math.floor((interval % day) / hour);
         const minuteNumber = Math.floor((interval % hour) / minute);
-        const secondNumber = Math.floor((interval % minute) / secund);
-    };
+        const secondNumber = Math.floor((interval % minute) / second);
 
+        setDay(dayNumber);
+        setHour(hourNumber);
+        setMinute(minuteNumber);
+        setSecond(secondNumber);
+    };
 
     setInterval(countdown, 1000);
 
-
-    return 1;
+    return [day, hour, minute, second];
 
 }
 
